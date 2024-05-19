e61adb2ee37c4e0bab30f6e7a835262c6ff7dec5
ali <alihgad2@gmail.com>
2024-05-19 23:16:04 +0300

    m

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 import express from 'express'
 import router from './items/items.routes.js'
 import cors from 'cors'
-var corsOptions = {
-    origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
+
 
 const app = express()
 const port = 3000 || process.env.PORT
 
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.json())
 app.use('/items',router)
 
