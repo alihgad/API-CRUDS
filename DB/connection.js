b5c6c8f60b0bd931b1ca36e9dee8a1f6cb82d3f0
ali <alihgad2@gmail.com>
2024-05-20 23:18:16 +0300

    error

--- DB/connection.js
+++ DB/connection.js
@@ -1,12 +1,6 @@
 import mysql from 'mysql2'
 
 let connect =  mysql.createConnection('mysql://items_purplebest:284b75c41423cf9115993d847e83b93c32e79b5b@8w3.h.filess.io:3307/items_purplebest')
-// let connect =  mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: 'items',
-// })
 
 
 connect.connect((err)=>{
